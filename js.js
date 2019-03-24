dda3d6483e97a3a49293488974c7e34432233d5f
Sivanesh <sivanesh.call@gmail.com>
2019-03-24 15:59:28 +0530

    Started Once again solved scopeRemover and Printing function

--- js.js
+++ js.js
@@ -1,261 +1,275 @@
-let code = []
-let scope = []
+let code = [];
+let scope = [];
 
+// async function tester() {
+//     await dictated.forEach(item => {
+//         console.log(item)
+//         processing(item)
+//     })
+//     printCode()
+// }
 
-
-
-async function tester() {
-    await dictated.forEach(item => {
-        console.log(item)
-        processing(item)
-    })
-    printCode()    
-}
-
-tester()
-
+// tester()
 
 // The if chain
 function processing(string) {
-    string = string.toLowerCase()
-    let strArray = string.split(" ")
-    switch (true) {
-        case string.startsWith('let'):
-            declaration(strArray.shift(), strArray)
-            break
-        case string.startsWith('constant'):
-            declaration('const', strArray.slice(1))
-            break
-        case string.startsWith('variable'):
-            declaration('var', strArray.slice(1))
-            break
-        case string.startsWith('function'):
-            functionCreator(strArray.slice(1))
-            break
-        case string.startsWith('arguments'):
-            assignArguments(strArray.slice(1))
-            break
-        case string.startsWith('close'):
-            scopeRemover()
-            break
-        case string.startsWith('undo'):
-            undo()
-            break
-        case string.startsWith('return'):
-            returnToFunction(strArray.slice(1))
-            break
-        case string.startsWith('operation'):
-            operationHandler(strArray.slice(1))
-            break
-        case string.startsWith('comment paragraph'):
-            commentCreator(strArray.slice(2))
-            break
-        case string.startsWith('comment'):
-            comment(strArray.slice(1))
-            break
-        case string.startsWith('object'):
-            objectCreator(strArray.slice(1))
-            break
-        default: 
-            simpleWordConversions(strArray) 
-    }
+	string = string.toLowerCase();
+	string = string.replace(/equals/g, '=');
+	let strArray = string.split(' ');
+	switch (true) {
+		case string.startsWith('let'):
+			declaration(strArray.shift(), strArray);
+			break;
+		case string.startsWith('constant'):
+			declaration('const', strArray.slice(1));
+			break;
+		case string.startsWith('variable'):
+			declaration('var', strArray.slice(1));
+			break;
+		case string.startsWith('function'):
+			functionCreator(strArray.slice(1));
+			break;
+		case string.startsWith('arguments'):
+			assignArguments(strArray.slice(1));
+			break;
+		case string.startsWith('close'):
+			scopeRemover();
+			break;
+		case string.startsWith('undo'):
+			undo();
+			break;
+		case string.startsWith('return'):
+			returnToFunction(strArray.slice(1));
+			break;
+		case string.startsWith('operation'):
+			operationHandler(strArray.slice(1));
+			break;
+		case string.startsWith('comment paragraph'):
+			commentCreator(strArray.slice(2));
+			break;
+		case string.startsWith('comment'):
+			comment(strArray.slice(1));
+			break;
+		case string.startsWith('object'):
+			objectCreator(strArray.slice(1));
+			break;
+		case string.startsWith('undo'):
+			undo();
+			break;
+		case string.startsWith('clear'):
+			clear();
+			break;
+		default:
+			simpleWordConversions(strArray);
+	}
 }
 
 function declaration(type, strArray) {
-    let equalsIndex = strArray.indexOf(`=`)
-    let variable, assignment
-    if (equalsIndex != -1) {
-        variable = strArray.slice(0, strArray.indexOf('='))
-        assignment = strArray.slice(strArray.indexOf("=") + 1)
-        assignment = typeDefiner(assignment.join(" "))
-    } else {
-        variable = strArray
-    }
-    variable = variable.join("_")
-    scopeAssigner(`${type} ${variable} = ${assignment}`)
-    // code.push(`${type} ${variable} = ${assignment}`)
-    // printCode()
+	let equalsIndex = strArray.indexOf(`=`);
+	let variable, assignment;
+	if (equalsIndex != -1) {
+		variable = strArray.slice(0, strArray.indexOf('='));
+		assignment = strArray.slice(strArray.indexOf('=') + 1);
+		assignment = typeDefiner(assignment.join(' '));
+	} else {
+		variable = strArray;
+	}
+	variable = variable.join('_');
+	scopeAssigner(`${type} ${variable} = ${assignment}`);
+	// code.push(`${type} ${variable} = ${assignment}`)
+	// printCode()
 }
 
 function typeDefiner(data) {
-    console.log(typeof data, data)
-    if(typeof data === 'string' && data.startsWith('list')) {
-        data = data.split(" ")
-        return listCreator(data.slice(1))
-    } else if(data.startsWith('string')) {
-        return `'${data.split(" ").slice(1).join(" ")}'`
-    } else if (!isNaN(data)) {
-        return Number(data)
-    } else if (data === "true" || data === "false") {
-        return data
-    } if(data.startsWith('object')) {
-        return objectCreator(data.split(" ").slice(1).join(" "))
-    } else {
-        return data.split(" ").join("_")
-    }
+	console.log(typeof data, data);
+	if (typeof data === 'string' && data.startsWith('list')) {
+		data = data.split(' ');
+		return listCreator(data.slice(1));
+	} else if (data.startsWith('string')) {
+		return `'${data.split(' ').slice(1).join(' ')}'`;
+	} else if (!isNaN(data)) {
+		return Number(data);
+	} else if (data === 'true' || data === 'false') {
+		return data;
+	}
+	if (data.startsWith('object')) {
+		return objectCreator(data.split(' ').slice(1).join(' '));
+	} else {
+		return data.split(' ').join('_');
+	}
 }
 
 function functionCreator(strArray) {
-    console.log(strArray)
-    let functionName = methodNameCreator(strArray)
-    let method = new Function(functionName, `function`)
-    scope.push(method)
+	console.log(strArray);
+	let functionName = methodNameCreator(strArray);
+	let method = new Function(functionName, `function`);
+	console.log('[JS] functionCreator', method);
+	scopeAssigner(method);
+	scope.push(method);
+	console.log(scope, code);
+	printCode();
 }
 
 function methodNameCreator(strArray) {
-    return strArray.map((name, index) => {
-        if (index != 0) {
-            return name[0].toUpperCase() + name.slice(1)
-        }
-        return name
-    }).join("")
+	return strArray
+		.map((name, index) => {
+			if (index != 0) {
+				return name[0].toUpperCase() + name.slice(1);
+			}
+			return name;
+		})
+		.join('');
 }
 
 function assignArguments(strArray) {
-    if(scope.length != 0) {
-        let args = strArray.join(" ").split("next")
-        args = args.map(item => item.trim().split(" ").join("_"))
-        scope[scope.length - 1].arguments.push(args)
-    } else {
-        console.error('Already in global scope')
-    }
+	if (scope.length != 0) {
+		let args = strArray.join(' ').split('next');
+		args = args.map((item) => item.trim().split(' ').join('_'));
+		scope[scope.length - 1].arguments.push(args);
+	} else {
+		console.error('Already in global scope');
+	}
 }
 
 function returnToFunction(strArray) {
-    if(scope.length == 0) {
-        console.error('You are in Global scope and can\'t return anything from there')
-        return
-    }
+	if (scope.length == 0) {
+		console.error("You are in Global scope and can't return anything from there");
+		return;
+	}
 
-    let i 
-    for(i = scope.length - 1; i >= 0; i--) {
-        if(scope[i].constructor.name == 'Function') {
-            scopeAssigner('return ' + strArray.join("_"))
-            return
-        }
-    }
-    
-    console.error('You are in Global scope and can\'t return anything from there')  
+	let i;
+	for (i = scope.length - 1; i >= 0; i--) {
+		if (scope[i].constructor.name == 'Function') {
+			scopeAssigner('return ' + strArray.join('_'));
+			return;
+		}
+	}
+
+	console.error("You are in Global scope and can't return anything from there");
 }
 
 function printCode() {
-    // console.clear()
-    NaNParser()
-    console.log(code.join("\n"))
-    console.log(`code`, code)
-    document.querySelector("#code").innerHTML = code.join("\n")
+	// console.clear()
+	console.log('[JS] PrintCode: called');
+	// NaNParser();
+	console.log(`code`, code, scope);
+	console.log(code.join('\n'));
+	document.querySelector('#code').innerHTML = code.join('\n');
 }
 
 function NaNParser() {
-    code = code.map(item => { 
-        return item.replace(/not_a_number/g, 'NaN')
-    })
+	code = code.map((item) => {
+		item = String(item);
+		return item.replace(/not_a_number/g, 'NaN');
+	});
 }
 
 function scopeAssigner(data) {
-    if (scope.length > 0) {
-        scope[scope.length - 1].body.push(data)
-    } else {
-        code.push(data)
-    }
+	if (scope.length > 0) {
+		scope[scope.length - 1].body.push(data);
+	} else {
+		code.push(data);
+	}
 
-    printCode()
+	console.log(`[JS] SCOPE ASSIGNER`, data);
+
+	printCode();
 }
 
 function undo() {
-    if(scope.length > 0) {
-        scope[scope.length - 1].body.pop()
-    } else {
-        code.pop()
-    }
-    printCode()
+	if (scope.length > 0) {
+		scope[scope.length - 1].body.pop();
+	} else {
+		code.pop();
+	}
+	printCode();
 }
 
 function scopeRemover() {
-    if(scope.length == 1) {
-        if(scope[scope.length - 1].type != 'function') {
-            return
-        }
-        code.push(scope.pop().builder())
-    } else if(scope.length > 1) {
-        let data = scope.pop().builder()
-        scope[scope.length - 1].body.push(data)
-    } else {
-        console.error('Already in Global scope')
-    }
-    printCode()
+	if (scope.length > 0) {
+		if (scope[scope.length - 1].type != 'function') {
+			return;
+		}
+		let function_data = scope.pop();
+		console.log('[JS] ScopeRemover: ', function_data);
+	} else {
+		console.error('Already in Global scope');
+	}
+	printCode();
 }
 
 function simpleWordConversions(strArray) {
-    let string = strArray.join(" ")
-    switch(true) {
-        case string.startsWith('console'):
-            consoling(strArray.slice(1))
-            break
-        default: 
-            // the original default
-            scopeAssigner(string)
-    }
+	let string = strArray.join(' ');
+	switch (true) {
+		case string.startsWith('console'):
+			consoling(strArray.slice(1));
+			break;
+		default:
+			// the original default
+			scopeAssigner(string);
+	}
 }
 
 function consoling(strArray) {
-    switch(true) {
-        case strArray.join(" ").startsWith('log'):
-            scopeAssigner(`console.log(${typeDefiner(strArray.slice(1).join(" "))})`)
-    }
+	switch (true) {
+		case strArray.join(' ').startsWith('log'):
+			scopeAssigner(`console.log(${typeDefiner(strArray.slice(1).join(' '))})`);
+	}
 }
 
 function operationHandler(operation) {
-    let str = operation.join(" ")
-    
-    str = str.replace(/bitwise and/g, '&')
-    str = str.replace(/bitwise or/g, '|')
-    str = str.replace(/bitwise not/g, '~')
-    str = str.replace(/left shift/g, '<<')
-    str = str.replace(/right shift/g, '>>')
+	let str = operation.join(' ');
 
-    str = str.replace(/and/g, '&&')
-    str = str.replace(/or/g, '||')
-    str = str.replace(/not/g, '!')
+	str = str.replace(/bitwise and/g, '&');
+	str = str.replace(/bitwise or/g, '|');
+	str = str.replace(/bitwise not/g, '~');
+	str = str.replace(/left shift/g, '<<');
+	str = str.replace(/right shift/g, '>>');
 
+	str = str.replace(/and/g, '&&');
+	str = str.replace(/or/g, '||');
+	str = str.replace(/not/g, '!');
 
-    str = str.replace(/not triple equals/g, '!==')
-    str = str.replace(/not equal to/g, '!=')
-    str = str.replace(/equal to/g, '==')
-    str = str.replace(/triple equals/g, '===')
-    str = str.replace(/greater than/g, '>')
-    str = str.replace(/less than/g, '<')
-    str = str.replace(/greater than equals/g, '>=')
-    str = str.replace(/less than equals/g, '<=')
+	str = str.replace(/not triple equals/g, '!==');
+	str = str.replace(/not equal to/g, '!=');
+	str = str.replace(/equal to/g, '==');
+	str = str.replace(/triple equals/g, '===');
+	str = str.replace(/greater than/g, '>');
+	str = str.replace(/less than/g, '<');
+	str = str.replace(/greater than equals/g, '>=');
+	str = str.replace(/less than equals/g, '<=');
 
+	str = str.replace(/plus equals/g, '+=');
+	str = str.replace(/minus equals/g, '-=');
+	str = str.replace(/into equals/g, '*=');
+	str = str.replace(/by equals/g, '/=');
+	str = str.replace(/percentage equals/g, '%=');
+	str = str.replace(/plus/g, '+');
+	str = str.replace(/minus/g, '-');
+	str = str.replace(/into/g, '*');
+	str = str.replace(/by/g, '/');
+	str = str.replace(/equals/g, '=');
+	str = str.replace(/increment/g, '++');
+	str = str.replace(/decrement/g, '--');
+	str = str.replace(/percentage/g, '%');
 
-
-
-    str = str.replace(/plus equals/g, '+=')
-    str = str.replace(/minus equals/g, '-=')
-    str = str.replace(/into equals/g, '*=')
-    str = str.replace(/by equals/g, '/=')
-    str = str.replace(/percentage equals/g, '%=')
-    str = str.replace(/plus/g, '+')
-    str = str.replace(/minus/g, '-')
-    str = str.replace(/into/g, '*')
-    str = str.replace(/by/g, '/')
-    str = str.replace(/equals/g, '=')
-    str = str.replace(/increment/g, '++')
-    str = str.replace(/decrement/g, '--')
-    str = str.replace(/percentage/g, '%')
-
-    scopeAssigner(str)
-}   
+	scopeAssigner(str);
+}
 
 function comment(data) {
-    scopeAssigner('// ' + data.join(" "))
+	scopeAssigner('// ' + data.join(' '));
 }
 
 function commentCreator(data) {
-    let comment = new MultiLineComment()
-    scope.push(comment)
-    data.forEach(item => {
-        scopeAssigner(item)
-    })
+	let comment = new MultiLineComment();
+	scope.push(comment);
+	data.forEach((item) => {
+		scopeAssigner(item);
+	});
+}
+
+function clear() {
+	code = [];
+	console.clear();
+	printCode();
 }
--- util.js
+++ util.js
@@ -1,77 +1,82 @@
 function Function(name, type) {
-    this.name = name
-    this.body = []
-    this.arguments = []
-    this.type = type
-    this.builder = function () {
-        this.body = this.body.map(item => `    ${item}`)
-        this.body.unshift(`function ${this.name} (${this.arguments.join(", ")}) {`)
-        this.body.push(`}`)
-        return this.toString()
-    }
+	this.name = name;
+	this.body = [];
+	this.arguments = [];
+	this.type = type;
+	// this.builder = function () {
+	//     this.body = this.body.map(item => `    ${item}`)
+	//     this.body.unshift(`function ${this.name} (${this.arguments.join(", ")}) {`)
+	//     this.body.push(`}`)
+	//     return this.toString()
+	// }
 }
 
 Function.prototype.toString = function() {
-    return this.body.join("\n")
+	let output = `
+function ${this.name} () {
+    ${this.body.map((item) => '    ' + item).join('\n')}
 }
+`;
+	return output;
+};
 
 function For() {
-    this.condition = ``
-    this.body = []
-    this.builder = function() {
-        this.body.unshift(`for(${this.condition}) {`)
-        this.body.push(`}`)
-        return this.toString()
-    }
+	this.condition = ``;
+	this.body = [];
+	this.builder = function() {
+		this.body.unshift(`for(${this.condition}) {`);
+		this.body.push(`}`);
+		return this.toString();
+	};
 }
 
-For.prototype.toString = function () {
-    return this.body.join("\n")
-}
+For.prototype.toString = function() {
+	return this.body.join('\n');
+};
 
 function List() {
-    this.name = ''
-    this.body = []
-    this.builder = function() {
-        this.body = `[${this.body.toString()}]`
-        this.body = this.body.replace(/,/g, ', ')
-    }
+	this.name = '';
+	this.body = [];
+	this.builder = function() {
+		this.body = `[${this.body.toString()}]`;
+		this.body = this.body.replace(/,/g, ', ');
+	};
 }
 
-List.prototype.toString = function () {
-    return this.body
-}
+List.prototype.toString = function() {
+	return this.body;
+};
 
 function Obj() {
-    this.name = ''
-    this.body = []
-    this.keyValuePair = function(key, value) {
-        key = key.split(" ").join("_")
-        value = typeDefiner(value)
-        this.body.push(`${key}: ${value}`)
-    }
+	this.name = '';
+	this.body = [];
+	this.keyValuePair = function(key, value) {
+		key = key.split(' ').join('_');
+		value = typeDefiner(value);
+		this.body.push(`${key}: ${value}`);
+	};
 
-    this.builder = function() {
-        this.body = this.body.map(item => `    ${item}`)
-        this.body = `{\n ${this.body.join(", \n")}\n}`
-        return this.toString()
-    }
+	this.builder = function() {
+		this.body = this.body.map((item) => `    ${item}`);
+		this.body = `{\n ${this.body.join(', \n')}\n}`;
+		return this.toString();
+	};
 }
 
 Obj.prototype.toString = function() {
-    return this.body
-}
+	return this.body;
+};
 
 function MultiLineComment() {
-    this.body = []
-    this.builder = function() {
-        this.body = `/*
-    ${this.body.join("\n")}
-*/`
-        return this.toString()
-    }
+	this.body = [];
+	this.builder = function() {
+		this.body = `/*
+    ${this.body.join('\n')}
+*/`;
+		return this.toString();
+	};
 }
 
 MultiLineComment.prototype.toString = function() {
-    return this.body
-}
+	return this.body;
+};
